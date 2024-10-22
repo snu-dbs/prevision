59165430bd616d46f4f5916d3cb9122f47892d57
Kyoseung Koo <koo@dbs.snu.ac.kr>
2024-10-22 15:12:48 +0900

    updated scidb import script

--- evaluation/scidb/guest/load-dense.sh
+++ evaluation/scidb/guest/load-dense.sh
@@ -1,23 +1,5 @@
 # /bin/bash
-DIR=../../../slab-benchmark/prevision/output/scidb/
-
-# 80000000x100_dense 
-iquery -aq "CREATE ARRAY mat_80Mx100_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
-iquery -aq "load(mat_80Mx100_dense_coo,'$DIR/80000000x100_dense_coo.csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_80Mx100_dense_coo, <value:double>[i=0:79999999:0:1000; j=0:99:0:1000]), mat_80Mx100_dense)"
-iquery -aq "remove(mat_80Mx100_dense_coo)"
-
-# 40000000x100_dense 
-iquery -aq "CREATE ARRAY mat_40Mx100_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
-iquery -aq "load(mat_40Mx100_dense_coo,'$DIR/40000000x100_dense_coo.csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_40Mx100_dense_coo, <value:double>[i=0:39999999:0:1000; j=0:99:0:1000]), mat_40Mx100_dense)"
-iquery -aq "remove(mat_40Mx100_dense_coo)"
-
-# 20000000x100_dense 
-iquery -aq "CREATE ARRAY mat_20Mx100_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
-iquery -aq "load(mat_20Mx100_dense_coo,'$DIR/20000000x100_dense_coo.csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_20Mx100_dense_coo, <value:double>[i=0:19999999:0:1000; j=0:99:0:1000]), mat_20Mx100_dense)"
-iquery -aq "remove(mat_20Mx100_dense_coo)"
+DIR=../../../slab-benchmark/prevision/output/scidb
 
 # 10000000x100_dense
 iquery -aq "CREATE ARRAY mat_10Mx100_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
@@ -25,17 +7,23 @@ iquery -aq "load(mat_10Mx100_dense_coo,'$DIR/10000000x100_dense_coo.csv', -2, 'C
 iquery -aq "store(redimension(mat_10Mx100_dense_coo, <value:double>[i=0:9999999:0:1000; j=0:99:0:1000]), mat_10Mx100_dense)"
 iquery -aq "remove(mat_10Mx100_dense_coo)"
 
-# 80000000x10_dense 
-iquery -aq "CREATE ARRAY mat_80Mx10_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
-iquery -aq "load(mat_80Mx10_dense_coo,'$DIR/80000000x10_dense_coo.csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_80Mx10_dense_coo, <value:double>[i=0:79999999:0:1000; j=0:9:0:1000]), mat_80Mx10_dense)"
-iquery -aq "remove(mat_80Mx10_dense_coo)"
+# 10000000x10_dense
+iquery -aq "CREATE ARRAY mat_10Mx10_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
+iquery -aq "load(mat_10Mx10_dense_coo,'$DIR/10000000x10_dense_coo.csv', -2, 'CSV');"
+iquery -aq "store(redimension(mat_10Mx10_dense_coo, <value:double>[i=0:9999999:0:1000; j=0:9:0:1000]), mat_10Mx10_dense)"
+iquery -aq "remove(mat_10Mx10_dense_coo)"
 
-# 40000000x10_dense 
-iquery -aq "CREATE ARRAY mat_40Mx10_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
-iquery -aq "load(mat_40Mx10_dense_coo,'$DIR/40000000x10_dense_coo.csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_40Mx10_dense_coo, <value:double>[i=0:39999999:0:1000; j=0:9:0:1000]), mat_40Mx10_dense)"
-iquery -aq "remove(mat_40Mx10_dense_coo)"
+# 10000000x1_dense
+iquery -aq "CREATE ARRAY mat_10Mx1_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
+iquery -aq "load(mat_10Mx1_dense_coo,'$DIR/10000000x1_dense_coo.csv', -2, 'CSV');"
+iquery -aq "store(redimension(mat_10Mx1_dense_coo, <value:double>[i=0:9999999:0:1000; j=0:0:0:1000]), mat_10Mx1_dense)"
+iquery -aq "remove(mat_10Mx1_dense_coo)"
+
+# 20000000x100_dense 
+iquery -aq "CREATE ARRAY mat_20Mx100_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
+iquery -aq "load(mat_20Mx100_dense_coo,'$DIR/20000000x100_dense_coo.csv', -2, 'CSV');"
+iquery -aq "store(redimension(mat_20Mx100_dense_coo, <value:double>[i=0:19999999:0:1000; j=0:99:0:1000]), mat_20Mx100_dense)"
+iquery -aq "remove(mat_20Mx100_dense_coo)"
 
 # 20000000x10_dense 
 iquery -aq "CREATE ARRAY mat_20Mx10_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
@@ -43,35 +31,48 @@ iquery -aq "load(mat_20Mx10_dense_coo,'$DIR/20000000x10_dense_coo.csv', -2, 'CSV
 iquery -aq "store(redimension(mat_20Mx10_dense_coo, <value:double>[i=0:19999999:0:1000; j=0:9:0:1000]), mat_20Mx10_dense)"
 iquery -aq "remove(mat_20Mx10_dense_coo)"
 
-# 10000000x10_dense
-iquery -aq "CREATE ARRAY mat_10Mx10_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
-iquery -aq "load(mat_10Mx10_dense_coo,'$DIR/10000000x10_dense_coo.csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_10Mx10_dense_coo, <value:double>[i=0:9999999:0:1000; j=0:9:0:1000]), mat_10Mx10_dense)"
-iquery -aq "remove(mat_10Mx10_dense_coo)"
-
-# 80000000x1_dense 
-iquery -aq "CREATE ARRAY mat_80Mx1_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
-iquery -aq "load(mat_80Mx1_dense_coo,'$DIR/80000000x1_dense_coo.csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_80Mx1_dense_coo, <value:double>[i=0:79999999:0:1000; j=0:0:0:1000]), mat_80Mx1_dense)"
-iquery -aq "remove(mat_80Mx1_dense_coo)"
-
-# 40000000x1_dense 
-iquery -aq "CREATE ARRAY mat_40Mx1_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
-iquery -aq "load(mat_40Mx1_dense_coo,'$DIR/40000000x1_dense_coo.csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_40Mx1_dense_coo, <value:double>[i=0:39999999:0:1000; j=0:0:0:1000]), mat_40Mx1_dense)"
-iquery -aq "remove(mat_40Mx1_dense_coo)"
-
 # 20000000x1_dense 
 iquery -aq "CREATE ARRAY mat_20Mx1_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
 iquery -aq "load(mat_20Mx1_dense_coo,'$DIR/20000000x1_dense_coo.csv', -2, 'CSV');"
 iquery -aq "store(redimension(mat_20Mx1_dense_coo, <value:double>[i=0:19999999:0:1000; j=0:0:0:1000]), mat_20Mx1_dense)"
 iquery -aq "remove(mat_20Mx1_dense_coo)"
 
-# 10000000x1_dense
-iquery -aq "CREATE ARRAY mat_10Mx1_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
-iquery -aq "load(mat_10Mx1_dense_coo,'$DIR/10000000x1_dense_coo.csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_10Mx1_dense_coo, <value:double>[i=0:9999999:0:1000; j=0:0:0:1000]), mat_10Mx1_dense)"
-iquery -aq "remove(mat_10Mx1_dense_coo)"
+# Uncomment if you want to test OOM
+# # 40000000x100_dense 
+# iquery -aq "CREATE ARRAY mat_40Mx100_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
+# iquery -aq "load(mat_40Mx100_dense_coo,'$DIR/40000000x100_dense_coo.csv', -2, 'CSV');"
+# iquery -aq "store(redimension(mat_40Mx100_dense_coo, <value:double>[i=0:39999999:0:1000; j=0:99:0:1000]), mat_40Mx100_dense)"
+# iquery -aq "remove(mat_40Mx100_dense_coo)"
+
+# # 40000000x10_dense 
+# iquery -aq "CREATE ARRAY mat_40Mx10_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
+# iquery -aq "load(mat_40Mx10_dense_coo,'$DIR/40000000x10_dense_coo.csv', -2, 'CSV');"
+# iquery -aq "store(redimension(mat_40Mx10_dense_coo, <value:double>[i=0:39999999:0:1000; j=0:9:0:1000]), mat_40Mx10_dense)"
+# iquery -aq "remove(mat_40Mx10_dense_coo)"
+
+# # 40000000x1_dense 
+# iquery -aq "CREATE ARRAY mat_40Mx1_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
+# iquery -aq "load(mat_40Mx1_dense_coo,'$DIR/40000000x1_dense_coo.csv', -2, 'CSV');"
+# iquery -aq "store(redimension(mat_40Mx1_dense_coo, <value:double>[i=0:39999999:0:1000; j=0:0:0:1000]), mat_40Mx1_dense)"
+# iquery -aq "remove(mat_40Mx1_dense_coo)"
+
+# # 80000000x100_dense 
+# iquery -aq "CREATE ARRAY mat_80Mx100_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
+# iquery -aq "load(mat_80Mx100_dense_coo,'$DIR/80000000x100_dense_coo.csv', -2, 'CSV');"
+# iquery -aq "store(redimension(mat_80Mx100_dense_coo, <value:double>[i=0:79999999:0:1000; j=0:99:0:1000]), mat_80Mx100_dense)"
+# iquery -aq "remove(mat_80Mx100_dense_coo)"
+
+# # 80000000x10_dense 
+# iquery -aq "CREATE ARRAY mat_80Mx10_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
+# iquery -aq "load(mat_80Mx10_dense_coo,'$DIR/80000000x10_dense_coo.csv', -2, 'CSV');"
+# iquery -aq "store(redimension(mat_80Mx10_dense_coo, <value:double>[i=0:79999999:0:1000; j=0:9:0:1000]), mat_80Mx10_dense)"
+# iquery -aq "remove(mat_80Mx10_dense_coo)"
+
+# # 80000000x1_dense 
+# iquery -aq "CREATE ARRAY mat_80Mx1_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
+# iquery -aq "load(mat_80Mx1_dense_coo,'$DIR/80000000x1_dense_coo.csv', -2, 'CSV');"
+# iquery -aq "store(redimension(mat_80Mx1_dense_coo, <value:double>[i=0:79999999:0:1000; j=0:0:0:1000]), mat_80Mx1_dense)"
+# iquery -aq "remove(mat_80Mx1_dense_coo)"
 
 # 10x100_dense
 iquery -aq "CREATE ARRAY mat_10x100_dense_coo <i:int64,j:int64,value:double>[idx=0:*];"
--- evaluation/scidb/guest/load-sparse.sh
+++ evaluation/scidb/guest/load-sparse.sh
@@ -1,5 +1,5 @@
 # /bin/bash
-DIR=../../../slab-benchmark/prevision/output/scidb/
+DIR=../../../slab-benchmark/prevision/output/scidb
 
 # Import 
 density=0.0125
@@ -44,44 +44,45 @@ iquery -aq "load(mat_100x1_sparse_"$density2"_coo,'$DIR/100x1_sparse_"$density".
 iquery -aq "store(redimension(mat_100x1_sparse_"$density2"_coo, <value:double NOT NULL>[i=0:99:0:100; j=0:0:0:1]), mat_100x1_sparse_"$density2")"
 iquery -aq "remove(mat_100x1_sparse_"$density2"_coo)"
 
-density=0.05
-density2=0_05
+# density=0.05
+# density2=0_05
 
-# 400000000x100_sparse 
-iquery -aq "CREATE ARRAY mat_400Mx100_sparse_"$density2"_coo <i:int64,j:int64,value:double NOT NULL>[idx=0:*];"
-iquery -aq "load(mat_400Mx100_sparse_"$density2"_coo,'$DIR/400000000x100_sparse_"$density".csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_400Mx100_sparse_"$density2"_coo, <value:double NOT NULL>[i=0:399999999:0:4000000; j=0:99:0:100]), mat_400Mx100_sparse_"$density2")"
-iquery -aq "remove(mat_400Mx100_sparse_"$density2"_coo)"
+# Uncomment if you want to test OOM
+# # 400000000x100_sparse 
+# iquery -aq "CREATE ARRAY mat_400Mx100_sparse_"$density2"_coo <i:int64,j:int64,value:double NOT NULL>[idx=0:*];"
+# iquery -aq "load(mat_400Mx100_sparse_"$density2"_coo,'$DIR/400000000x100_sparse_"$density".csv', -2, 'CSV');"
+# iquery -aq "store(redimension(mat_400Mx100_sparse_"$density2"_coo, <value:double NOT NULL>[i=0:399999999:0:4000000; j=0:99:0:100]), mat_400Mx100_sparse_"$density2")"
+# iquery -aq "remove(mat_400Mx100_sparse_"$density2"_coo)"
 
-# 400000000x1_sparse 
-iquery -aq "CREATE ARRAY mat_400Mx1_sparse_"$density2"_coo <i:int64,j:int64,value:double NOT NULL>[idx=0:*];"
-iquery -aq "load(mat_400Mx1_sparse_"$density2"_coo,'$DIR/400000000x1_sparse_"$density".csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_400Mx1_sparse_"$density2"_coo, <value:double NOT NULL>[i=0:399999999:0:4000000; j=0:0:0:1]), mat_400Mx1_sparse_"$density2")"
-iquery -aq "remove(mat_400Mx1_sparse_"$density2"_coo)"
+# # 400000000x1_sparse 
+# iquery -aq "CREATE ARRAY mat_400Mx1_sparse_"$density2"_coo <i:int64,j:int64,value:double NOT NULL>[idx=0:*];"
+# iquery -aq "load(mat_400Mx1_sparse_"$density2"_coo,'$DIR/400000000x1_sparse_"$density".csv', -2, 'CSV');"
+# iquery -aq "store(redimension(mat_400Mx1_sparse_"$density2"_coo, <value:double NOT NULL>[i=0:399999999:0:4000000; j=0:0:0:1]), mat_400Mx1_sparse_"$density2")"
+# iquery -aq "remove(mat_400Mx1_sparse_"$density2"_coo)"
 
-# 100x1_sparse
-iquery -aq "CREATE ARRAY mat_100x1_sparse_"$density2"_coo <i:int64,j:int64,value:double NOT NULL>[idx=0:*];"
-iquery -aq "load(mat_100x1_sparse_"$density2"_coo,'$DIR/100x1_sparse_"$density".csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_100x1_sparse_"$density2"_coo, <value:double NOT NULL>[i=0:99:0:100; j=0:0:0:1]), mat_100x1_sparse_"$density2")"
-iquery -aq "remove(mat_100x1_sparse_"$density2"_coo)"
+# # 100x1_sparse
+# iquery -aq "CREATE ARRAY mat_100x1_sparse_"$density2"_coo <i:int64,j:int64,value:double NOT NULL>[idx=0:*];"
+# iquery -aq "load(mat_100x1_sparse_"$density2"_coo,'$DIR/100x1_sparse_"$density".csv', -2, 'CSV');"
+# iquery -aq "store(redimension(mat_100x1_sparse_"$density2"_coo, <value:double NOT NULL>[i=0:99:0:100; j=0:0:0:1]), mat_100x1_sparse_"$density2")"
+# iquery -aq "remove(mat_100x1_sparse_"$density2"_coo)"
 
-density=0.1
-density2=0_1
+# density=0.1
+# density2=0_1
 
-# 400000000x100_sparse 
-iquery -aq "CREATE ARRAY mat_400Mx100_sparse_"$density2"_coo <i:int64,j:int64,value:double NOT NULL>[idx=0:*];"
-iquery -aq "load(mat_400Mx100_sparse_"$density2"_coo,'$DIR/400000000x100_sparse_"$density".csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_400Mx100_sparse_"$density2"_coo, <value:double NOT NULL>[i=0:399999999:0:4000000; j=0:99:0:100]), mat_400Mx100_sparse_"$density2")"
-iquery -aq "remove(mat_400Mx100_sparse_"$density2"_coo)"
+# # 400000000x100_sparse 
+# iquery -aq "CREATE ARRAY mat_400Mx100_sparse_"$density2"_coo <i:int64,j:int64,value:double NOT NULL>[idx=0:*];"
+# iquery -aq "load(mat_400Mx100_sparse_"$density2"_coo,'$DIR/400000000x100_sparse_"$density".csv', -2, 'CSV');"
+# iquery -aq "store(redimension(mat_400Mx100_sparse_"$density2"_coo, <value:double NOT NULL>[i=0:399999999:0:4000000; j=0:99:0:100]), mat_400Mx100_sparse_"$density2")"
+# iquery -aq "remove(mat_400Mx100_sparse_"$density2"_coo)"
 
-# 400000000x1_sparse 
-iquery -aq "CREATE ARRAY mat_400Mx1_sparse_"$density2"_coo <i:int64,j:int64,value:double NOT NULL>[idx=0:*];"
-iquery -aq "load(mat_400Mx1_sparse_"$density2"_coo,'$DIR/400000000x1_sparse_"$density".csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_400Mx1_sparse_"$density2"_coo, <value:double NOT NULL>[i=0:399999999:0:4000000; j=0:0:0:1]), mat_400Mx1_sparse_"$density2")"
-iquery -aq "remove(mat_400Mx1_sparse_"$density2"_coo)"
+# # 400000000x1_sparse 
+# iquery -aq "CREATE ARRAY mat_400Mx1_sparse_"$density2"_coo <i:int64,j:int64,value:double NOT NULL>[idx=0:*];"
+# iquery -aq "load(mat_400Mx1_sparse_"$density2"_coo,'$DIR/400000000x1_sparse_"$density".csv', -2, 'CSV');"
+# iquery -aq "store(redimension(mat_400Mx1_sparse_"$density2"_coo, <value:double NOT NULL>[i=0:399999999:0:4000000; j=0:0:0:1]), mat_400Mx1_sparse_"$density2")"
+# iquery -aq "remove(mat_400Mx1_sparse_"$density2"_coo)"
 
-# 100x1_sparse
-iquery -aq "CREATE ARRAY mat_100x1_sparse_"$density2"_coo <i:int64,j:int64,value:double NOT NULL>[idx=0:*];"
-iquery -aq "load(mat_100x1_sparse_"$density2"_coo,'$DIR/100x1_sparse_"$density".csv', -2, 'CSV');"
-iquery -aq "store(redimension(mat_100x1_sparse_"$density2"_coo, <value:double NOT NULL>[i=0:99:0:100; j=0:0:0:1]), mat_100x1_sparse_"$density2")"
-iquery -aq "remove(mat_100x1_sparse_"$density2"_coo)"
+# # 100x1_sparse
+# iquery -aq "CREATE ARRAY mat_100x1_sparse_"$density2"_coo <i:int64,j:int64,value:double NOT NULL>[idx=0:*];"
+# iquery -aq "load(mat_100x1_sparse_"$density2"_coo,'$DIR/100x1_sparse_"$density".csv', -2, 'CSV');"
+# iquery -aq "store(redimension(mat_100x1_sparse_"$density2"_coo, <value:double NOT NULL>[i=0:99:0:100; j=0:0:0:1]), mat_100x1_sparse_"$density2")"
+# iquery -aq "remove(mat_100x1_sparse_"$density2"_coo)"
