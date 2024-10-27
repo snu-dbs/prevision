2ea860e0937f50f655012945511a1e7d2a69c77b
Kyoseung Koo <koo@dbs.snu.ac.kr>
2024-10-27 17:17:10 +0900

    updated scidb scripts

--- evaluation/scidb/exp_parallel_nmf.sh
+++ evaluation/scidb/exp_parallel_nmf.sh
@@ -4,7 +4,7 @@ SCRIPT_PATH="/prevision/evaluation/scidb/guest/alg-remote.sh"
 
 function init() {
 	sudo docker start $DOCKER_NAME
-	sleep 16
+	sleep 10
 
 	echo "Database Initialization"
 	sudo docker exec -it $DOCKER_NAME bash -c "chown scidb /dbpath; chmod a+x /prevision/evaluation/scidb/guest/*"
@@ -12,6 +12,7 @@ function init() {
 	sudo docker exec -it $DOCKER_NAME sudo -u scidb bash -c "cp /prevision/evaluation/scidb/config/""$CONFIG"" /opt/scidb/19.11/etc/config.ini"
 	sudo docker exec -it $DOCKER_NAME sudo -u scidb bash -c "echo 'y' | /opt/scidb/19.11/bin/scidbctl.py init-cluster"
 	sudo docker exec -it $DOCKER_NAME sudo -u scidb bash -c "/opt/scidb/19.11/bin/scidbctl.py start"
+	sleep 10
 
 	echo "Dataset Load"
 	sudo docker exec -it $DOCKER_NAME sudo -u scidb bash -c "PATH=/opt/scidb/19.11/bin:$PATH /prevision/evaluation/scidb/guest/load-dense-10m.sh"
@@ -34,7 +35,7 @@ function exp() {
         echo "###############################################"
 
 		sudo docker start $DOCKER_NAME
-		sleep 16
+		sleep 10
 		sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
 		sudo docker exec -it $DOCKER_NAME bash $SCRIPT_PATH $1 $2 $noi
 		sudo docker stop $DOCKER_NAME
